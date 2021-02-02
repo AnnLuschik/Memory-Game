53bebb3429b7f40d5c06e21b00509d10830cd6dc
Hanna Lushchyk <iniusha@gmail.com>
2021-02-02 12:42:21 +0300

    feat: add new card images

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,15 +1,53 @@
-import flag1 from './cards/flag-albania.jpg';
-import flag2 from './cards/flag-argentina.jpg';
-import flag3 from './cards/flag-belgium.jpg';
-import flag4 from './cards/flag-canada.jpg';
-import flag5 from './cards/flag-czech-republic.jpg';
-import flag6 from './cards/flag-egypt.jpg';
-import flag7 from './cards/flag-georgia.jpg';
-import flag8 from './cards/flag-great-britain.jpg';
-import flag9 from './cards/flag-lebanon.jpg';
-import flag10 from './cards/flag-mexico.jpg';
-import flag11 from './cards/flag-switzerland.jpg';
-import flag12 from './cards/flag-turkey.jpg';
+import flag1 from './cards/flag-albania.png';
+import flag2 from './cards/flag-argentina.png';
+import flag3 from './cards/flag-belgium.png';
+import flag4 from './cards/flag-canada.png';
+import flag5 from './cards/flag-czech-republic.png';
+import flag6 from './cards/flag-egypt.png';
+import flag7 from './cards/flag-georgia.png';
+import flag8 from './cards/flag-great-britain.png';
+import flag9 from './cards/flag-lebanon.png';
+import flag10 from './cards/flag-mexico.png';
+import flag11 from './cards/flag-switzerland.png';
+import flag12 from './cards/flag-turkey.png';
+import flag13 from './cards/flag-afghanistan.png';
+import flag14 from './cards/flag-algeria.png';
+import flag15 from './cards/flag-andorra.png';
+import flag16 from './cards/flag-angola.png';
+import flag17 from './cards/flag-antigua-and-barbuda.png';
+import flag18 from './cards/flag-armenia.png';
+import flag19 from './cards/flag-australia.png';
+import flag20 from './cards/flag-austria.png';
+import flag21 from './cards/flag-azerbaijan.png';
+import flag22 from './cards/flag-bahamas.png';
+import flag23 from './cards/flag-bahrain.png';
+import flag24 from './cards/flag-bangladesh.png';
+import flag25 from './cards/flag-barbados.png';
+import flag26 from './cards/flag-belize.png';
+import flag27 from './cards/flag-benin.png';
+import flag28 from './cards/flag-bolivia.png';
+import flag29 from './cards/flag-bosnia-and-herzegovina.png';
+import flag30 from './cards/flag-botswana.png';
+import flag31 from './cards/flag-bulgaria.png';
+import flag32 from './cards/flag-chile.png';
+import flag33 from './cards/flag-croatia.png';
+import flag34 from './cards/flag-ecuador.png';
+import flag35 from './cards/flag-estonia.png';
+import flag36 from './cards/flag-finland.png';
+import flag37 from './cards/flag-france.png';
+import flag38 from './cards/flag-jamaica.png';
+import flag39 from './cards/flag-japan.png';
+import flag40 from './cards/flag-kiribati.png';
+import flag41 from './cards/flag-liechtenstein.png';
+import flag42 from './cards/flag-mongolia.png';
+import flag43 from './cards/flag-nicaragua.png';
+import flag44 from './cards/flag-peru.png';
+import flag45 from './cards/flag-portugal.png';
+import flag46 from './cards/flag-serbia.png';
+import flag47 from './cards/flag-spain.png';
+import flag48 from './cards/flag-sweden.png';
+import flag49 from './cards/flag-thailand.png';
+import flag50 from './cards/flag-brazil.png';
 
 import name1 from './cards/name-albania.png';
 import name2 from './cards/name-argentina.png';
@@ -23,6 +61,44 @@ import name9 from './cards/name-lebanon.png';
 import name10 from './cards/name-mexico.png';
 import name11 from './cards/name-switzerland.png';
 import name12 from './cards/name-turkey.png';
+import name13 from './cards/name-afghanistan.png';
+import name14 from './cards/name-algeria.png';
+import name15 from './cards/name-andorra.png';
+import name16 from './cards/name-angola.png';
+import name17 from './cards/name-antigua-and-barbuda.png';
+import name18 from './cards/name-armenia.png';
+import name19 from './cards/name-australia.png';
+import name20 from './cards/name-austria.png';
+import name21 from './cards/name-azerbaijan.png';
+import name22 from './cards/name-bahamas.png';
+import name23 from './cards/name-bahrain.png';
+import name24 from './cards/name-bangladesh.png';
+import name25 from './cards/name-barbados.png';
+import name26 from './cards/name-belize.png';
+import name27 from './cards/name-benin.png';
+import name28 from './cards/name-bolivia.png';
+import name29 from './cards/name-bosnia-and-herzegovina.png';
+import name30 from './cards/name-botswana.png';
+import name31 from './cards/name-bulgaria.png';
+import name32 from './cards/name-chile.png';
+import name33 from './cards/name-croatia.png';
+import name34 from './cards/name-ecuador.png';
+import name35 from './cards/name-estonia.png';
+import name36 from './cards/name-finland.png';
+import name37 from './cards/name-france.png';
+import name38 from './cards/name-jamaica.png';
+import name39 from './cards/name-japan.png';
+import name40 from './cards/name-kiribati.png';
+import name41 from './cards/name-liechtenstein.png';
+import name42 from './cards/name-mongolia.png';
+import name43 from './cards/name-nicaragua.png';
+import name44 from './cards/name-peru.png';
+import name45 from './cards/name-portugal.png';
+import name46 from './cards/name-serbia.png';
+import name47 from './cards/name-spain.png';
+import name48 from './cards/name-sweden.png';
+import name49 from './cards/name-thailand.png';
+import name50 from './cards/name-brazil.png';
 
 export interface IImage {
   key: string
@@ -44,6 +120,44 @@ export const flags:Images = [
   { key: 'mexico', path: flag10 },
   { key: 'switzerland', path: flag11 },
   { key: 'turkey', path: flag12 },
+  { key: 'afghanistan', path: flag13 },
+  { key: 'algeria', path: flag14 },
+  { key: 'andorra', path: flag15 },
+  { key: 'angola', path: flag16 },
+  { key: 'antigua-and-barbuda', path: flag17 },
+  { key: 'armenia', path: flag18 },
+  { key: 'australia', path: flag19 },
+  { key: 'austria', path: flag20 },
+  { key: 'azerbaijan', path: flag21 },
+  { key: 'bahamas', path: flag22 },
+  { key: 'bahrain', path: flag23 },
+  { key: 'bangladesh', path: flag24 },
+  { key: 'barbados', path: flag25 },
+  { key: 'belize', path: flag26 },
+  { key: 'benin', path: flag27 },
+  { key: 'bolivia', path: flag28 },
+  { key: 'bosnia-and-herzegovina', path: flag29 },
+  { key: 'botswana', path: flag30 },
+  { key: 'bulgaria', path: flag31 },
+  { key: 'chile', path: flag32 },
+  { key: 'croatia', path: flag33 },
+  { key: 'ecuador', path: flag34 },
+  { key: 'estonia', path: flag35 },
+  { key: 'finland', path: flag36 },
+  { key: 'france', path: flag37 },
+  { key: 'jamaica', path: flag38 },
+  { key: 'japan', path: flag39 },
+  { key: 'kiribati', path: flag40 },
+  { key: 'liechtenstein', path: flag41 },
+  { key: 'mongolia', path: flag42 },
+  { key: 'nicaragua', path: flag43 },
+  { key: 'peru', path: flag44 },
+  { key: 'portugal', path: flag45 },
+  { key: 'serbia', path: flag46 },
+  { key: 'spain', path: flag47 },
+  { key: 'sweden', path: flag48 },
+  { key: 'thailand', path: flag49 },
+  { key: 'brazil', path: flag50 },
 ];
 
 export const names:Images = [
@@ -59,4 +173,42 @@ export const names:Images = [
   { key: 'mexico', path: name10 },
   { key: 'switzerland', path: name11 },
   { key: 'turkey', path: name12 },
+  { key: 'afghanistan', path: name13 },
+  { key: 'algeria', path: name14 },
+  { key: 'andorra', path: name15 },
+  { key: 'angola', path: name16 },
+  { key: 'antigua-and-barbuda', path: name17 },
+  { key: 'armenia', path: name18 },
+  { key: 'australia', path: name19 },
+  { key: 'austria', path: name20 },
+  { key: 'azerbaijan', path: name21 },
+  { key: 'bahamas', path: name22 },
+  { key: 'bahrain', path: name23 },
+  { key: 'bangladesh', path: name24 },
+  { key: 'barbados', path: name25 },
+  { key: 'belize', path: name26 },
+  { key: 'benin', path: name27 },
+  { key: 'bolivia', path: name28 },
+  { key: 'bosnia-and-herzegovina', path: name29 },
+  { key: 'botswana', path: name30 },
+  { key: 'bulgaria', path: name31 },
+  { key: 'chile', path: name32 },
+  { key: 'croatia', path: name33 },
+  { key: 'ecuador', path: name34 },
+  { key: 'estonia', path: name35 },
+  { key: 'finland', path: name36 },
+  { key: 'france', path: name37 },
+  { key: 'jamaica', path: name38 },
+  { key: 'japan', path: name39 },
+  { key: 'kiribati', path: name40 },
+  { key: 'liechtenstein', path: name41 },
+  { key: 'mongolia', path: name42 },
+  { key: 'nicaragua', path: name43 },
+  { key: 'peru', path: name44 },
+  { key: 'portugal', path: name45 },
+  { key: 'serbia', path: name46 },
+  { key: 'spain', path: name47 },
+  { key: 'sweden', path: name48 },
+  { key: 'thailand', path: name49 },
+  { key: 'brazil', path: name50 },
 ];
