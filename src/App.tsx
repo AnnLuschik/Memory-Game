2bc75663fc5591d55cb090a5a7a2e53109757242
Hanna Lushchyk <iniusha@gmail.com>
2021-02-02 14:06:03 +0300

    feat: add components for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  LoginPage, MainPage, GamePage, RecordsPage,
+} from './pages';
+import { RootState, bootstrapStart } from './state';
+
+const PublicRoute = () => (
+  <Switch>
+    <Route exact path="/">
+      <Redirect to="/game" />
+    </Route>
+    <Route exact path="/game" component={MainPage} />
+    <Route exact path="/game/play" component={GamePage} />
+    <Route exact path="/game/records" component={RecordsPage} />
+    <Route exact path="/not-found" />
+    <Redirect to="/not-found" />
+  </Switch>
+);
 
 export function App() {
-  return (
-    <Switch>
-      <Route exact path="/" />
-      <Route exact path="/login" />
-      <Route exact path="/game" />
-      <Route exact path="/game/play" />
-      <Route exact path="/game/records" />
-      <Route exact path="/not-found" />
-      <Redirect to="/not-found" />
-    </Switch>
-  );
+  const { bootstrapped, isAuth } = useSelector((state: RootState) => state.login);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(bootstrapStart());
+  }, [dispatch]);
+
+  return bootstrapped
+    ? (
+      <>
+        <Route
+          render={() => {
+            if (isAuth) {
+              return <PublicRoute />;
+            }
+            return <Redirect to="/login" />;
+          }}
+        />
+        <Route exact path="/login" component={LoginPage} />
+      </>
+    ) : null;
 }
